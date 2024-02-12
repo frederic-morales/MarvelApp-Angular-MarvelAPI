5a2bc76208f11ebd1b406b8eab362d4258739c9e
frederic-morales <tocomorales30@gmail.com>
2024-02-11 20:13:02 -0600

    Terminando el footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-  redes = ['fa-github', 'fa-instagram', 'fa-twitter', 'fa-linkedin'];
+  redes = [
+    {
+      brand: 'fa-github',
+      link: 'https://github.com/frederic-morales',
+    },
+    {
+      brand: 'fa-instagram',
+      link: 'https://www.instagram.com/smoraless_fa/',
+    },
+    {
+      brand: 'fa-linkedin',
+      link: 'https://www.linkedin.com/in/frederic-soc-morales-8a1745261/',
+    },
+    {
+      brand: 'fa-twitter',
+      link: 'https://www.instagram.com/smoraless_fa/',
+    },
+  ];
 }
